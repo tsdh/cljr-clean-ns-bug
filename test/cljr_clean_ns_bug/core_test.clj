bd0f719ca6efe487d9acf8e9563bacd822b5cef2
Tassilo Horn <tsdh@gnu.org>
2015-07-07 16:17:56 +0200

    delete test/

--- test/cljr_clean_ns_bug/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns cljr-clean-ns-bug.core-test
-  (:require [clojure.test :refer :all]
-            [cljr-clean-ns-bug.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
